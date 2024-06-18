1946533f076a966e19a567624a7260f09f0329b9
TuneMeIn <robokidrb@outlook.com>
2024-06-19 10:58:16 +1200

    Commit update

--- Prototype V1.py
+++ Prototype V1.py
@@ -38,10 +38,6 @@ def check_inputs ():
     if len(entry_name.get()) == 0 :
         Label(main_window,fg="red", text="Required") .grid(column=2,row=0)
         input_check = 1
-    #Check that location is not blank, set error text if blank     
-    if len(entry_location.get()) == 0 :
-        Label(main_window,fg="red", text="Required") .grid(column=2,row=1)
-        input_check = 1
     #Check the item hired is not blank and between 5 and 10, set error text if blank  
     if (entry_item.get().isdigit()) : 
         if  int(entry_item.get()) < 5 or  int(entry_item.get()) > 10:
@@ -59,13 +55,13 @@ def check_inputs ():
 #add the next customer to the list
 def append_name ():
     #append each item to its own area of the list
-    customer_details.append([entry_name.get(),entry_location.get(),entry_item.get(),entry_weather.get()])
+    customer_details.append([entry_name.get(),entry_item.get(),entry_weather.get()])
     #clear the boxes
     entry_name.delete(0,'end')
-    entry_location.delete(0,'end')
     entry_item.delete(0,'end')
     entry_weather.delete(0,'end')
     counters['total_entries'] += 1
+    Label(main_window, text=counters['total_entries'], font=("Helvetica 10 bold")).grid(column=1,row=1)
 
 #delete a row from the list
 def delete_row ():
@@ -88,6 +84,7 @@ def setup_buttons():
     #create all the empty and default labels, buttons and entry boxes. Put them in the correct grid location
     Label(main_window, text="Customer Name") .grid(column=0,row=0,sticky=E)
     Label(main_window, text="Receipt Number") .grid(column=0,row=1,sticky=E)
+    Label(main_window, text=counters['total_entries'], font=("Helvetica 10 bold")).grid(column=1,row=1)
     Button(main_window, text="Quit",font=("Arial 9 bold"),command=quit,bg="crimson",fg="white",width = 10) .grid(column=4, row=0,sticky=E)
     Button(main_window, text="Append Details",command=check_inputs) .grid(column=3,row=1)
     Button(main_window, text="Print Details",command=print_customer_details,width = 10) .grid(column=4,row=1,sticky=E)
@@ -109,8 +106,6 @@ customer_details = []
 main_window =Tk()    
 entry_name = Entry(main_window)
 entry_name.grid(column=1,row=0)
-entry_location = Entry(main_window)
-entry_location.grid(column=1,row=1)
 entry_item = Entry(main_window)
 entry_item.grid(column=1,row=2)
 entry_weather = Entry(main_window)
